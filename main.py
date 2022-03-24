ba3d383b38c6c98b1a5ac3c75f53b7afa636a59c
= <=>
2022-03-24 06:54:35 +0100

    main window

---
+++ main.py
@@ -0,0 +1,86 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QTimer, QTime, Qt
+from PyQt5.QtGui import QPixmap
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("Timer To The Moon")
+        self.UI()
+        self.show()
+    def UI(self):
+        self.mainDesign()
+        self.layouts()
+
+    def mainDesign(self):
+        self.setStyleSheet("background-color:rgb(18, 52, 121);color: white; font-family")
+        self.title_for_main = QLabel("Clocky To The Moon")
+        self.title_for_main.setStyleSheet("font-size:30pt")
+        self.lineEdit_for_name = QLineEdit()
+        self.lineEdit_for_name.setPlaceholderText(" Please Enter your Name")
+        self.lineEdit_for_name.setStyleSheet("font-size:20pt")
+        self.clockLabel = QLabel()
+        self.clockLabel.setStyleSheet("font-size:30pt")
+        self.logoLabel = QLabel()
+        self.logoLabel.setPixmap(QPixmap("images/roc_klein.gif"))
+        #
+        self.title_for_main.setAlignment(Qt.AlignCenter)
+
+        self.logoLabel.setAlignment(Qt.AlignBottom)
+        self.clockLabel.setAlignment(Qt.AlignCenter)
+
+
+
+
+
+
+    def layouts(self):
+        self.mainLayout = QVBoxLayout()
+        self.topLayout = QHBoxLayout()
+        self.centerLayout = QHBoxLayout()
+        self.buttomLayout = QHBoxLayout()
+        self.logoLayout = QHBoxLayout()
+
+        ##
+
+        self.mainLayout.addLayout(self.topLayout)
+        self.mainLayout.addLayout(self.centerLayout)
+        self.mainLayout.addLayout(self.buttomLayout)
+        self.mainLayout.addLayout(self.logoLayout)
+
+        self.setLayout(self.mainLayout)
+        #######
+        self.topLayout.addWidget((self.title_for_main))
+        self.centerLayout.addWidget((self.lineEdit_for_name))
+        self.buttomLayout.addWidget(self.clockLabel)
+        self.logoLayout.addWidget(self.logoLabel)
+        self.centerLayout.setContentsMargins(200,20,200,30)
+        timer = QTimer(self)
+        timer.timeout.connect(self.myClock)
+        timer.start(1000)
+
+
+    def myClock(self):
+        current_time = QTime.currentTime()
+
+        label_time = current_time.toString('hh:mm:ss')
+
+        self.clockLabel.setText(label_time)
+
+
+
+
+
+def main():
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec_())
+
+
+if __name__ == "__main__":
+    main()
