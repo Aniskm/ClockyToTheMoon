f309c14dbc666ce49ef91e110344689065332109
= <=>
2022-03-27 00:26:00 +0100

    Add Second Window and Database

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QTimer, QTime, Qt
 from PyQt5.QtGui import QPixmap
+import sqlite3
+
+con = sqlite3.connect("clocky.db")
+cur = con.cursor()
 
 
 class Window(QWidget):
@@ -73,6 +77,8 @@ class Window(QWidget):
 
     def accountCheck(self):
         ## check not finisched
+        self.uName = self.lineEdit_for_name.text()
+        self.getUsers()
         self.windowForWork()
 
     def windowForWork(self):
@@ -80,18 +86,93 @@ class Window(QWidget):
         self.close()
 
 
+    def getUsers(self):
+        query = "SELECT user_name FROM users"
+        users_list = cur.execute(query).fetchall()
+
+        print(users_list)
+        if users_list:
+            for user in users_list:
+                print("lol")
+
+                if user[0] == self.uName:
+                    print("test")
+                    print("Hallo {}".format(self.uName))
+                else:
+                    print("you are new here")
+                    self.addUsers()
+        else:
+            print("call add user")
+            self.addUsers()
+
+    def addUsers(self):
+        print("called")
+        name = self.uName
+        try:
+            print("try")
+            query = "INSERT INTO users (user_name) VALUES(?)"
+            cur.execute(query, name)
+            con.commit()
+            print("work ")
+        except:
+            print("d ont work")
+
+
 class WindowForWork(QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle("Project Management")
-        self.UI()
+        self.ui()
         self.show()
 
-    def UI(self):
+    def ui(self):
+        self.mainDesign()
+        self.layouts()
+    def mainDesign(self):
+        self.listWidget = QListWidget()
+        self.btnNew = QPushButton("New")
+        self.btnUpdate = QPushButton("Update")
+        self.btnDelete = QPushButton("Delete")
+        self.clockimage = QLabel()
+        self.clockimage.setPixmap(QPixmap("images/clocki.png"))
+        self.nameLabel = QLabel()
+        self.projectNameLabel = QLabel("Project")
+        self.monthNameLabel = QLabel("Month")
+        self.projectComboBoxe= QComboBox()
+        self.monthComboBoxe = QComboBox()
+        self.months_list()
+        self.projectListUpdate()
+
+    def layouts(self):
+        self.mainLayout = QHBoxLayout()
+        self.leftayout = QFormLayout()
+        self.rightMainLayout = QVBoxLayout()
+        self.rightTopLayout = QHBoxLayout()
+        self.rightBottomLayout = QHBoxLayout()
+        ###
+        self.rightMainLayout.addLayout(self.rightTopLayout)
+        self.rightMainLayout.addLayout(self.rightBottomLayout)
+        self.mainLayout.addLayout(self.leftayout,40)
+        self.mainLayout.addLayout(self.rightMainLayout,60)
+        ###
+        self.rightTopLayout.addWidget(self.listWidget)
+        self.rightBottomLayout.addWidget(self.btnNew)
+        self.rightBottomLayout.addWidget(self.btnUpdate)
+        self.rightBottomLayout.addWidget(self.btnDelete)
+        ###
+        self.leftayout.addRow(" ",self.clockimage)
+        self.leftayout.addRow("",self.nameLabel)
+        self.leftayout.addRow(self.projectNameLabel,self.projectComboBoxe)
+        self.leftayout.addRow(self.monthNameLabel,self.monthComboBoxe)
+        self.setLayout(self.mainLayout)
+
+    def months_list(self):
+        months =['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+        for month in months:
+            self.monthComboBoxe.addItem(month)
+    def projectListUpdate(self):
         pass
-
-
 def main():
     App = QApplication(sys.argv)
     window = Window()
